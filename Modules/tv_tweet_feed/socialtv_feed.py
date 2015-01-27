424a39d92d7ac5a4e26d229a0bc59da3e24c1bb9
hrishipatil12 <hrishipatil12@gmail.com>
2015-01-26 22:51:07 -0800

    Initial commit

---
+++ Modules/tv_tweet_feed/socialtv_feed.py
@@ -0,0 +1,37 @@
+#python script to fetch and store(JSON format) social tv feed for tv shows
+#Test search : Fetches tweets for the tv show "The Mentalist" 
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import time
+
+# Please refer your twitter app details and insert the values below
+ckey=''
+csecret=''
+atoken=''
+asecret=''
+
+class listener(StreamListener):
+    def on_data(self,data):
+        try:
+            print data
+        
+            #storing(appending) the data in twitDB.csv file
+            saveFile=open('twitDB.csv','a')
+            saveFile.write(data)
+            saveFile.write('\n')
+            saveFile.close()
+            return True
+        except BaseException,e:
+            print 'failed ondata,',str(e)
+            time.sleep(5)     
+    
+    def on_error(self,status):
+        print status
+
+auth= OAuthHandler(ckey,csecret)
+auth.set_access_token(atoken,asecret)
+twitterStream=Stream(auth,listener())
+twitterStream.filter(track=["tvshows"])
+#try replacing the above tracking string with any other search string
+# like if you want feed for the tv show "The Mentalist", just replace the above string with "#TheMentalist"                  
